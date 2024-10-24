0582a0eb9f3d6132c53785b75fbb885018667295
katasiimes <o2sika01@students.oamk.fi>
2024-10-24 19:03:38 +0300

    functionality + styling

--- vite-project/src/components/image.jsx
+++ vite-project/src/components/image.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import "./css/image.css"
 
 export const Image = (props) => {
   return (
--- vite-project/src/pages/dogs.jsx
+++ vite-project/src/pages/dogs.jsx
@@ -2,27 +2,39 @@ import React from 'react'
 import { useEffect, useState } from 'react'
 import axios from 'axios';
 import { Image } from '../components/image';
+import "../components/css/image.css"
 
 export const Dogs = () => {
-    
+
     const [ImageURL, setImageURL] = useState();
+    const [bgColor, setBgColor] = useState('white');
     const APIURL = "https://random.dog/woof.json"
-    
+
 
     useEffect(() => {
-      axios.get(APIURL).then((response) => {
-        setImageURL(response.data["url"])
+        axios.get(APIURL).then((response) => {
+            setImageURL(response.data["url"])
 
-      });
+        });
 
     }, [])
-    
 
 
+const handleColorChange = (color) => {
+    setBgColor(color);
+};
 
-  return (
-    <div>
-        <Image RandomImage={ImageURL}/>
-    </div>
-  )
-}
+    return (
+        <div style={{ backgroundColor: bgColor, minHeight: '100vh' }}>
+            <div className="color-picker-menu">
+                <button className="color-button color-blue" onClick={() => handleColorChange('lightblue')}>Blue</button>
+                <button className="color-button color-green" onClick={() => handleColorChange('lightgreen')}>Green</button>
+                <button className="color-button color-red" onClick={() => handleColorChange('lightcoral')}>Red</button>
+                <button className="color-button color-yellow" onClick={() => handleColorChange('gold')}>Yellow</button>
+            </div>
+            <div>
+                <Image RandomImage={ImageURL} />
+            </div>
+        </div>
+    );
+};
