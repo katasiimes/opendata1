845d82380886b87e5c845579e25791c8024d2f75
katasiimes <o2sika01@students.oamk.fi>
2024-10-26 17:32:40 +0300

    fixing mistakes in page and  renaming

--- vite-project/src/pages/Page.jsx
+++ vite-project/src/pages/Page.jsx
@@ -1,44 +1,41 @@
-import React, { useState } from 'react';
-import Header from '../components/Header.jsx';
-import ProductForm from '../components/ProductForm';
-import OrderInfo from '../components/OrderInfo';
-
+import React, { useState } from 'react'
+import Header from '../components/Header.jsx'
+import ProductForm from '../components/ProductForm'
+import OrderInfo from '../components/OrderInfo'
 
 const Page = () => {
-  const items = ['Item 1', 'Item 2']
-  const prices = [245.0, 350.0]
+    const products = ['Product 1', 'Product 2']
+    const prices = [245.0, 350.0]
 
-  const [itemIndex, setItemIndex] = useState(0);
-
-  const [count, setCount] = useState(1)
-
-  const handleItemChange = (index) => {
-    setItemIndex(index)
-  }
-
-
-  const handleCountChange = (newCount) => {
-    setCount(newCount)
-  }
-
-  return (
-    <div className="main-ui">
-      <Header title="Product Page" />
-
-      <ProductForm
-        item={items}
-        prices={prices}
-        onItemChange={handleItemChange}
-        onCountChange={handleCountChange}
-      />
-
-      <OrderInfo
-        itemName={items[itemIndex]}
-        price={prices[itemIndex]}
-        count={count}
-      />
-    </div>
-  );
-};
-
-export default Page;
+    const [selectedProduct, setSelectedProduct] = useState(0);
+    const [qty, setQty] = useState(1)
+  
+    const handleProductChange = (index) => {
+      setSelectedProduct(index)
+    };
+  
+    const handleQtyChange = (newQty) => {
+      setQty(newQty)
+    };
+  
+    return (
+      <div className="main-ui">
+        <Header title="Product Page" />
+  
+        <ProductForm
+          products={products}
+          prices={prices}
+          onProductChange={handleProductChange}
+          onQtyChange={handleQtyChange}
+        />
+  
+        <OrderInfo
+          productName={products[selectedProduct]}
+          price={prices[selectedProduct]}
+          qty={qty}
+        />
+      </div>
+    )
+}
+  
+  export default Page
